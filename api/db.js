f3fbc3d8ef587aa67924e1c12d5daad3a47fa8da
Dominik Ucher <dominikucher@hotmail.com>
2025-05-01 11:40:26 +0200

    db conflict fixed

--- api/db.js
+++ api/db.js
@@ -3,20 +3,13 @@ dotenv.config();
 
 import mysql from 'mysql2';
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    // socketPath: "/var/run/mysqld/mysqld.sock",
-    connectionLimit: 20,     // Max 10 connections at a time
+    socketPath: "/var/run/mysqld/mysqld.sock",
+    waitForConnections: true,
+    connectionLimit: 20,
     queueLimit: 0,
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-    } else {
-        console.log('Connected to the database successfully!');
-    }
 });
